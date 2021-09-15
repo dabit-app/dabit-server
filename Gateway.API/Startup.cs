b638d24c4a58332d01b9d4bfa9bc713be8ee7c83
Maxime Bonvin <max.bonvin@gmail.com>
2021-09-15 18:51:41 +0200

    allow authorization header to accept jwt bearer

--- Gateway.API/Startup.cs
+++ Gateway.API/Startup.cs
@@ -30,7 +30,7 @@ namespace Gateway.API
                     "CorsPolicy",
                     builder => builder
                         .AllowAnyOrigin()
-                        .WithHeaders("content-type")
+                        .WithHeaders("content-type", "authorization")
                 );
             });
 
